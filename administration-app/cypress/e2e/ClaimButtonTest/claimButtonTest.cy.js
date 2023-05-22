cec4423975a1221e36c4c1cbff5ef246c3c4593c
isthisfahret <73430835+isthisfahret@users.noreply.github.com>
2023-05-23 00:51:05 +0200

    Minor adjustments to GUI test

--- administration-app/cypress/e2e/ClaimButtonTest/claimButtonTest.cy.js
+++ administration-app/cypress/e2e/ClaimButtonTest/claimButtonTest.cy.js
@@ -2,10 +2,6 @@
 
 describe('Testing claims form', () => {
     beforeEach(() => {
-      // Cypress starts out with a blank slate for each test
-      // so we must tell it to visit our website with the `cy.visit()` command.
-      // Since we want to visit the same URL at the start of all our tests,
-      // we include it in our beforeEach function so that it runs before each test
       cy.on('uncaught:exception', (err, runnable) => {
         return false;
       })
@@ -13,31 +9,20 @@ describe('Testing claims form', () => {
       cy.visit('http://digipay.cc/login');
     })
   
-    it('claims', () => {
+    it('Testing if filing a claim works as expected', () => {
         cy.get(':nth-child(3) > .MuiInputBase-input').type("evlahovlja1@etf.unsa.ba");
         cy.get(':nth-child(4) > .MuiInputBase-input').type("Sifra!23");
         cy.get('.login-btn').click();
         cy.get(':nth-child(6) > .MuiButtonBase-root').click();
         cy.get('.MuiList-root > [tabindex="0"]').click();
-        cy.wait(4000);
-        cy.get('.MuiTypography-h2 > .MuiButtonBase-root').click();
-        cy.wait(4000);
-        cy.get('.css-j0q1ur').should('exist');
-        cy.get(':nth-child(5) > :nth-child(8) > .MuiButtonBase-root').click();
-        cy.wait(4000);
-        cy.get('.css-11sx3g7').should('exist');
-        cy.get('.MuiInputBase-input').eq(8).type('I am not in danger');
+        cy.get('.css-1a4b21o').eq(6).click();
+        cy.get('.MuiInputBase-input').eq(8).type('I am not in danger Skyler');
         cy.get('.MuiInputBase-input').eq(9).type('I am the danger');
         cy.get('.MuiDropzoneArea-textContainer').attachFile('walterwhite.pdf', { subjectType: 'drag-n-drop' });
-        cy.wait(6000);
+        cy.wait(5000);
         cy.get('.MuiButtonBase-root').contains('File claim').click();
-        cy.get('.MuiTypography-h2 > .MuiButtonBase-root').click();
-        cy.get('.css-j0q1ur').should('exist');
         cy.visit('http://digipay.cc/transaction/claims');
-        cy.get('.MuiTableCell-root').contains('I am not in danger').should('exist');
-
-
-        
+        cy.get('.MuiTableCell-root').contains('I am not in danger Skyler').should('exist');
     })
     
 });
